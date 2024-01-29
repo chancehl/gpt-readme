d8c4b29bfc7ae4a3114c995aef3d87bc037b4e95
Chance Linz <clinz.dev@gmail.com>
2024-01-28 21:20:53 -0800

    adding clap and argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
+use clap::Parser;
 use dotenv::dotenv;
 use reqwest::{Client, Error};
 use serde::{Deserialize, Serialize};
-use std::{collections::HashMap, env};
+use std::{env, fs, path::PathBuf};
+
+/// A program to generate a README for your project with ChatGPT.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The path to your project
+    #[arg(short, long)]
+    path: PathBuf,
+
+    /// Where to save the README file to
+    #[arg(short, long)]
+    out: PathBuf,
+}
 
 #[derive(Serialize, Deserialize)]
 struct Root {
     model: String,
     messages: Vec<Message>,
-    tools: Vec<Tool>,
-    tool_choice: String,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -17,89 +29,42 @@ struct Message {
     content: String,
 }
 
-#[derive(Serialize, Deserialize)]
-struct Tool {
-    #[serde(rename = "type")]
-    tool_type: String,
-    function: Function,
-}
-
-#[derive(Serialize, Deserialize)]
-struct Function {
-    name: String,
-    description: String,
-    parameters: Parameters,
-}
-
-#[derive(Serialize, Deserialize)]
-struct Parameters {
-    #[serde(rename = "type")]
-    param_type: String,
-    properties: HashMap<String, Property>,
-    required: Vec<String>,
-}
-
-#[derive(Serialize, Deserialize)]
-struct Property {
-    #[serde(rename = "type")]
-    property_type: String,
-    description: Option<String>,
-    #[serde(rename = "enum")]
-    enum_values: Option<Vec<String>>,
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    // Initialize .env file
     dotenv().ok();
 
+    // Parse args
+    let args = Args::parse();
+
+    // Pull diff
+    let diff = fs::read_to_string("./diff.txt").expect("Could not read diff.txt file");
+
+    // Read key
     let openai_api_key = env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY not set");
 
+    // Define client
     let client = Client::new();
 
-    let message = Message {
+    // Define system message
+    let system_message = Message {
+        role: "system".to_string(),
+        content: "You are a bot who generates markdown code for README.md files based on a user-provided git diff. Please include relevant information to the project including (but not limited to) setup instructions (based on the technologies used) and installation steps. Please include links to relevant information and documentation.".to_string(),
+    };
+
+    // Define user message
+    let user_message = Message {
         role: "user".to_string(),
-        content: "What is the weather like in Boston?".to_string(),
+        content: diff,
     };
 
-    let function_parameters = Parameters {
-        param_type: "object".to_string(),
-        properties: {
-            let mut properties = HashMap::new();
-
-            properties.insert(
-                "diff".to_string(),
-                Property {
-                    property_type: "string".to_string(),
-                    description: Some(
-                        "The output of the git diff command for a given repository".to_string(),
-                    ),
-                    enum_values: None,
-                },
-            );
-
-            properties
-        },
-        required: vec!["diff".to_string()],
-    };
-
-    let tool_function = Function {
-        name: "get_current_weather".to_string(),
-        description: "Get the current weather in a given location".to_string(),
-        parameters: function_parameters,
-    };
-
-    let tool = Tool {
-        tool_type: "function".to_string(),
-        function: tool_function,
-    };
-
+    // Define body
     let body = Root {
         model: "gpt-3.5-turbo".to_string(),
-        messages: vec![message],
-        tools: vec![tool],
-        tool_choice: "auto".to_string(),
+        messages: vec![system_message, user_message],
     };
 
+    // Execute http request
     let response = client
         .post("https://api.openai.com/v1/chat/completions")
         .header("Content-Type", "application/json")
